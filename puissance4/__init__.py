ef5675e4bfc3166217ad0f0abfa5c57aa2d2a481
Elian Ollivier <elian.ollivier@etu.unilim.fr>
2025-09-11 16:21:47 +0200

    premier test chaine cicd

--- puissance4/__init__.py
+++ puissance4/__init__.py
@@ -1,9 +1,18 @@
 __all__ = [
     "ModelePuissance4",
     "ControleurJeu",
-    "VueTk",
 ]
 
 from .controller import ControleurJeu
 from .model import ModelePuissance4
-from .view import VueTk
+
+# Import de la vue GUI de manière optionnelle pour éviter
+# les erreurs en environnement CI/serveur sans Tkinter.
+try:
+    from .view import VueTk  # type: ignore
+except Exception:
+    # L'import échoue si Tkinter n'est pas disponible; ce n'est pas bloquant
+    # pour l'utilisation du modèle/contrôleur ni pour les tests.
+    pass
+else:
+    __all__.append("VueTk")
